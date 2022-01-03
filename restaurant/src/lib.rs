e49e9607ecc97846508d963629515ed6f3c2525b
Bart Krakowski <bartlomiej@krakowski.dev>
2022-01-03 16:08:51 +0100

    feat: module system

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,58 @@
+use rand::{Rng, CryptoRng};
+use std::io::{self, Write};
+// use std::io::*;
+
+mod front_of_house;
+
+
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     front_of_house::hosting::add_to_waitlist();
+// }
+
+fn serve_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+
+    let order1 = back_of_house::Appetizer::Soup;
+
+    pub use crate::front_of_house::hosting;
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    hosting::add_to_waitlist();
+}
