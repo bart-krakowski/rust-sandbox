bb23478abab78397d45a6aef1578a3f0e54d4a54
Bart Krakowski <bartlomiej@krakowski.dev>
2022-01-03 17:15:21 +0100

    feat: error handling

---
+++ error_handling/src/main.rs
@@ -0,0 +1,47 @@
+use std::{fs::{File, read_to_string}, io::{ErrorKind, Error}};
+
+fn main() {
+    let file = File::open("hello.txt");
+
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+
+    let file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+
+
+    // let file = File::open("hello.txt").unwrap();
+
+    // let file = match file {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("Problem opening the file: {:?}", error),
+    // };
+
+    let file = File::open("hello.txt").expect("Failed to open the file: {:?}");
+
+
+    // Error propagation
+
+    fn read_username_from_file() -> Result<String, Error> {
+        let mut s = String::new();
+        // File::open("hello.txt")?.read_to_string(&mut s)?;
+
+         read_to_string("hello.txt")
+    }
+}
