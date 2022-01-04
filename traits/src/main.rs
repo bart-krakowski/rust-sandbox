354446dc97efc9053572fee6643f6ce7767ec5b6
Bart Krakowski <bartlomiej@krakowski.dev>
2022-01-04 15:52:34 +0100

    feat: traits

---
+++ traits/src/main.rs
@@ -0,0 +1,107 @@
+use std::fmt::{Debug, Display};
+
+pub struct NewsArticle {
+    pub author: String,
+    pub head: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.head, self.author, self.content)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    let article = NewsArticle {
+        author: String::from("John Doe"),
+        head: String::from("The sky is falling"),
+        content: String::from("The sky is not actually falling."),
+    };
+
+    // notify(&article);
+
+    returns_summarizable().summarize();
+}
+
+pub fn notify(item1: &(impl Summary + Display), item2: &impl Summary) {}
+
+// pub fn notify<T: Summary + Display>(item1: &T, item2: T) {}
+
+fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) {}
+
+fn some_function2<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    2
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl <T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+// impl<T: Display> ToString for T {
+
+// }
