42529ae3ae3f65a309da9085b961d41a1979ad74
Bart Krakowski <bartlomiej@krakowski.dev>
2022-01-10 16:16:05 +0100

    refactor: minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,44 +1,19 @@
-use std::{env, fs, process, error::Error};
+use std::{env, process};
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config {query, filename})
-    }
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(&args).unwrap_or_else(|err: &str| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for: {}", config.query);
-    println!("In file: {}", config.filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
